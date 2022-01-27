d4d1d7ec70375d94b4d450e07717ca19a8d3444c
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-01-27 16:31:20 +0000

    project created

---
+++ src/main/java/com/jv/fcsilva/projeccrud/ProjecCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jv.fcsilva.projeccrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jv/fcsilva/projeccrud/ProjecCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv.fcsilva.projeccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
