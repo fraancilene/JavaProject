8e68283c1cf0703e7f739a89e3ae367cdf81c8a8
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-01-31 22:51:16 +0000

    OrderStatus Enum

--- src/main/java/com/jv/fcsilva/projectcrud/config/Config.java
+++ src/main/java/com/jv/fcsilva/projectcrud/config/Config.java
@@ -2,6 +2,7 @@ package com.jv.fcsilva.projectcrud.config;
 
 import com.jv.fcsilva.projectcrud.entity.Order;
 import com.jv.fcsilva.projectcrud.entity.User;
+import com.jv.fcsilva.projectcrud.entity.enums.OrderStatus;
 import com.jv.fcsilva.projectcrud.repository.OrderRepository;
 import com.jv.fcsilva.projectcrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class Config implements CommandLineRunner {
         User u2 = new User(null, "Anne", "anne@gmail.com", "9877521", "125456");
         //User u3 = new User(null, "Joana", "anne@gmail.com", "9877521", "125456");
 
-        Order o1 = new Order(null,Instant.parse("2022-01-20T13:54:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2022-01-20T13:54:07Z"), u2);
-        Order o3 = new Order(null,Instant.parse("2022-01-20T13:54:07Z"), u1);
+        Order o1 = new Order(null,Instant.parse("2022-01-20T13:54:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2022-01-20T13:54:07Z"),OrderStatus.WAITTING_PAYMENT, u2);
+        Order o3 = new Order(null,Instant.parse("2022-01-20T13:54:07Z"), OrderStatus.WAITTING_PAYMENT, u1);
 
         // salvando no banco de dados
         userRepository.saveAll(Arrays.asList(u1, u2));
--- src/main/java/com/jv/fcsilva/projectcrud/entity/Order.java
+++ src/main/java/com/jv/fcsilva/projectcrud/entity/Order.java
@@ -1,17 +1,11 @@
 package com.jv.fcsilva.projectcrud.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import com.jv.fcsilva.projectcrud.entity.enums.OrderStatus;
 import javax.persistence.*;
 import java.time.Instant;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "tb_order")
 public class Order {
 
@@ -22,7 +16,54 @@ public class Order {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
 }
